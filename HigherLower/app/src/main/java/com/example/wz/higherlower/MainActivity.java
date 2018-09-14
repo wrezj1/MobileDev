90b172940c05ecf68a16c2f1c4e62a8b226c8e11
Wrezj Kebabdji <wrezj.kebabdji@hva.nl>
2018-09-14 12:17:03 +0200

    added comments

--- HigherLower/app/src/main/java/com/example/wz/higherlower/MainActivity.java
+++ HigherLower/app/src/main/java/com/example/wz/higherlower/MainActivity.java
@@ -15,95 +15,114 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonHigher;
-    Button buttonLower;
+    //declaration of object
+    private Button buttonHigher;
+    private Button buttonLower;
 
-    TextView textViewScore;
-    TextView textViewHighScore;
-    ImageView imageViewDice;
-    ListView scoreList;
+    private TextView textViewScore;
+    private TextView textViewHighScore;
+    private ImageView imageViewDice;
+    private ListView scoreList;
 
-    ArrayList<String> scoreListArray = new ArrayList<>();
-    ArrayAdapter<String> arrayAdapter;
-    ArrayList<String> a;
+    private ArrayList<String> scoreListArray = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter;
+
+    //declaration of variables
+     private int currentRoll;
+     private int diceRoll = 0;
+     private int scoreCounter = 0;
+     private int highScoreCounter = 0;
+     //dice sides
+     private final int SIDES = 6;
 
 
-     int currentRoll;
-     int diceRoll = 0;
-     int scoreCounter = 0;
-     int highScoreCounter = 0;
-
-
-
+    /**
+     *
+     * @param savedInstanceState
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialize first dice
         if(currentRoll == 0){
             currentRoll = roll();
             setImage(currentRoll);
-
-
         }
 
+        //initializing objects with view
         buttonHigher = (Button) findViewById(R.id.buttonHigher);
         buttonLower = (Button) findViewById(R.id.buttonLower);
 
+        textViewScore = (TextView) findViewById(R.id.textViewScore);
+        textViewHighScore = (TextView) findViewById(R.id.textViewHighScore);
+
+
+
+        textViewScore = (TextView) findViewById(R.id.textViewScore);
+        textViewHighScore = (TextView) findViewById(R.id.textViewHighScore);
+
         scoreList = (ListView) findViewById(R.id.scoreList);
 
-        textViewScore = (TextView) findViewById(R.id.textViewScore);
-        textViewHighScore = (TextView) findViewById(R.id.textViewHighScore);
-
-        imageViewDice = (ImageView) findViewById(R.id.imageViewDice);
-
-        textViewScore = (TextView) findViewById(R.id.textViewScore);
-        textViewHighScore = (TextView) findViewById(R.id.textViewHighScore);
-
-
-
-
+        //creating arrayAdapter linked to list with score history
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 scoreListArray);
 
-        View.OnClickListener myClickLIstener= new View.OnClickListener() {
+        //General listener
+        View.OnClickListener generalListener= new View.OnClickListener() {
             String outcome;
             public void onClick(View v) {
 
-
+                //rolling the dice
                 diceRoll = roll();
+                setImage(diceRoll);
 
+                //get the pressed button tag
                 String buttonInput = (String) v.getTag();
 
+                //linking score list with score listView
+                scoreList.setAdapter(arrayAdapter);
+
+                //start rolling
                 outcome = startRoll(currentRoll,diceRoll,buttonInput);
                 scoreCounter(outcome);
-                setImage(diceRoll);
+
+                //set currentRoll to skip first check
                 currentRoll = diceRoll;
 
+                //show message of the outcome
                 Toast.makeText(MainActivity.this,outcome,Toast.LENGTH_SHORT).show();
 
+                //initializing score & highscore view
                 textViewScore.setText("Score: "+scoreCounter);
                 textViewHighScore.setText("Highscore: "+highScoreCounter);
-                scoreList.setAdapter(arrayAdapter);
+
+
             }
         };
 
-
+        //setting tags buttons & linking to our general listener
         buttonLower.setTag("LOWER");
-        buttonLower.setOnClickListener(myClickLIstener);
+        buttonLower.setOnClickListener(generalListener);
         buttonHigher.setTag("HIGHER");
-        buttonHigher.setOnClickListener(myClickLIstener);
+        buttonHigher.setOnClickListener(generalListener);
 
     }
 
-
+    /**
+     *
+     * @param diceRoll
+     * setting the imageViewDice equal to the result of diceRoll
+     */
     private void setImage(int diceRoll){
         imageViewDice = (ImageView) findViewById(R.id.imageViewDice);
+
         switch (diceRoll){
             case 1:
                 imageViewDice.setImageResource(R.drawable.dice1);
                 break;
-
             case 2:
                 imageViewDice.setImageResource(R.drawable.dice2);
                 break;
@@ -120,11 +139,14 @@ public class MainActivity extends AppCompatActivity {
                 imageViewDice.setImageResource(R.drawable.dice6);
                 break;
         }
-
-
-
     }
 
+    /**
+     *
+     * @param outcome
+     * When guessed correct, scoreCounter +1
+     * When highscore lower than score, replace highscore with score
+     */
     private void scoreCounter(String outcome){
         if (outcome.equals("Winner!")) {
             scoreCounter ++;
@@ -137,6 +159,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     *
+     * @param currentRoll
+     * @param diceRoll
+     * @param buttonInput
+     * @return message outcome guess
+     */
     private String startRoll(int currentRoll,int diceRoll, String buttonInput) {
         String result;
 
@@ -150,21 +179,19 @@ public class MainActivity extends AppCompatActivity {
             result = "No Winners, No Losers";
         }
 
-
+        //adding result to score list
         scoreListArray.add("Throw is: " + diceRoll);
 
         return result;
     }
 
+    /**
+     *
+     * @return number between 1 and 6
+     */
     private int roll() {
-        int SIDES = 6;
-        int roll = (int) (Math.random() * SIDES) + 1;
-
-
-        return roll;
+        int i= (int) (Math.random() * SIDES) + 1;
+        return i;
     }
-
-
-
 }
 
